8dee81e0562158650e1f419b50b02e5969324df9
GuilherRinaldi <guilherme.rinaldi@outlook.com>
2021-08-01 11:40:16 -0300

    repetição e escolha das pastas

--- organizer.py
+++ organizer.py
@@ -1,6 +1,7 @@
-import os, shutil, re
 
-path = 'C:\\Users\\'
+import os, shutil, re, time
+
+paths = ('C:\\Users\\**\\Downloads\\','C:\\Users\\**\\Documents')
 types = {'txt':'text',
          'pdf':'pdf',
          'mp3':'audio',
@@ -16,7 +17,7 @@ types = {'txt':'text',
          ('rar','zip'):'compressed'}
 show_movie_regex = re.compile(r'\d{3,4}[pP]|[sS]\d{1,2}[eE]\d{1,2}')
 
-def make_folder(foldername):
+def make_folder(foldername,path):
     os.chdir(path)
     if os.path.exists(foldername) == True:
         return os.getcwd() + os.sep + str(foldername)
@@ -30,23 +31,26 @@ def move_to_new_folder(src_path,path_to_new_folder):
     except:
         pass
 
-def foldername(name,is_file):
+def foldername(name):
     if show_movie_regex.search(name):
         return 'movie and show'
-    elif is_file:
+    else:
         for value in types.keys():
             if name[name.rindex('.')+1:] in value:
                 return types[value]
         return 'other'
 
-with os.scandir(path) as it:
-    for entry in it:
-        if not entry.name.startswith('.'):
-            print('d')
-            try:
-                move_to_new_folder(entry.path, make_folder(foldername(entry.name,entry.is_file)))
-            except:
-                pass
 
-""" watchdog """
+while True:
+    for path in paths:
+        with os.scandir(path) as it:
+            for entry in it:
+                if not entry.name.startswith('.'):
+                    try:
+                        move_to_new_folder(entry.path, make_folder(foldername(entry.name),path))
+                    except:
+                        pass
+    time.sleep(1)
+
+
 
